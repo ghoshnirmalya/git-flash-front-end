447bda3c1230458ee167c9647ee287f1eb00759a
Nirmalya Ghosh <nirmalya.email@gmail.com>
2020-09-07 23:13:32 +0530

    feat: adds the option to compare images

--- packages/app-dashboard/pages/index.tsx
+++ packages/app-dashboard/pages/index.tsx
@@ -1,6 +1,8 @@
 import { Box, Grid, Image } from "@chakra-ui/core";
 import { PrismaClient } from "@prisma/client";
 import { NextPage } from "next";
+import React, { useEffect, useState } from "react";
+import compareImages from "resemblejs/compareImages";
 
 interface screenshot {
   id: string;
@@ -12,11 +14,43 @@ interface IProps {
 }
 
 const IndexPage: NextPage<IProps> = ({ screenshots }) => {
+  const [diffImage, setDiffImage] = useState("");
+
+  useEffect(() => {
+    const screenshotOne = `data:image/png;base64,${screenshots[0].image}`;
+    const screenshotTwo = `data:image/png;base64,${screenshots[1].image}`;
+
+    const options = {
+      output: {
+        errorColor: {
+          red: 255,
+          green: 0,
+          blue: 255,
+        },
+        errorType: "movement",
+        transparency: 1,
+        largeImageThreshold: 1200,
+        useCrossOrigin: false,
+        outputDiff: true,
+      },
+      scaleToSameSize: true,
+    };
+
+    const performImageComparison = async () => {
+      const data = await compareImages(screenshotOne, screenshotTwo, options);
+      const image = await data.getImageDataUrl();
+
+      setDiffImage(image);
+    };
+
+    performImageComparison();
+  }, []);
+
   return (
     <>
       <Box p={4}>
         <Box maxW="6xl" mx="auto" w="full">
-          <Grid templateColumns="repeat(4, 1fr)" gap={4}>
+          <Grid templateColumns="repeat(3, 1fr)" gap={4}>
             {screenshots.map((screenshot) => {
               return (
                 <Box key={screenshot.id}>
@@ -28,6 +62,9 @@ const IndexPage: NextPage<IProps> = ({ screenshots }) => {
                 </Box>
               );
             })}
+            <Box>
+              <Image src={diffImage} rounded="md" shadow="lg" />
+            </Box>
           </Grid>
         </Box>
       </Box>
@@ -38,7 +75,7 @@ const IndexPage: NextPage<IProps> = ({ screenshots }) => {
 export async function getStaticProps() {
   const prisma = new PrismaClient();
 
-  const screenshots = await prisma.screenshot.findMany();
+  const screenshots = await prisma.screenshot.findMany({ take: 2 });
 
   return {
     props: {
