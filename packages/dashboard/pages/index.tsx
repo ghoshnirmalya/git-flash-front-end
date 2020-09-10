db8b9b4e782ac7eb1af87f1dca2ca5df42a6cfd9
Nirmalya Ghosh <nirmalya.email@gmail.com>
2020-09-10 18:34:06 +0530

    fix: fixes sls pages

--- packages/dashboard/pages/index.tsx
+++ packages/dashboard/pages/index.tsx
@@ -1,8 +1,7 @@
 import { Box, Grid, Image } from "@chakra-ui/core";
 import { PrismaClient } from "@prisma/client";
 import { NextPage } from "next";
-import React, { useEffect, useState } from "react";
-import compareImages from "resemblejs/compareImages";
+import React, { useState } from "react";
 
 interface screenshot {
   id: string;
@@ -19,36 +18,6 @@ const IndexPage: NextPage<IProps> = ({ screenshots }) => {
   }
 
   const [diffImage, setDiffImage] = useState("");
-
-  useEffect(() => {
-    const screenshotOne = `data:image/png;base64,${screenshots[0].image}`;
-    const screenshotTwo = `data:image/png;base64,${screenshots[1].image}`;
-
-    const options = {
-      output: {
-        errorColor: {
-          red: 255,
-          green: 0,
-          blue: 255,
-        },
-        errorType: "movement",
-        transparency: 1,
-        largeImageThreshold: 1200,
-        useCrossOrigin: false,
-        outputDiff: true,
-      },
-      scaleToSameSize: true,
-    };
-
-    const performImageComparison = async () => {
-      const data = await compareImages(screenshotOne, screenshotTwo, options);
-      const image = await data.getImageDataUrl();
-
-      setDiffImage(image);
-    };
-
-    performImageComparison();
-  }, []);
 
   return (
     <>
